dbe9098407533803749876d816c75c5fa365bf4f
NehalSurti <nehalsurti9998@gmail.com>
2024-03-10 22:38:59 +0000

    Updating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import styles from "./page.module.css";
 import Navbar from "@/sections/Navbar";
 import SocialIcons from "@/components/SocialIcons";
 import Email from "@/components/Email";
+import Hero from "@/sections/Hero";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar></Navbar>
+      <Hero></Hero>
       <SocialIcons></SocialIcons>
       <Email></Email>
     </main>
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,31 @@
+import Button from "@/components/Button";
+import Link from "next/link";
+import React from "react";
+// import { motion } from "framer-motion";
+function Hero() {
+  return (
+    <div className="hero">
+      <h1 className="hero-title">Hi my name is</h1>
+      <h2 className="hero-title-large">Kishan Sheth.</h2>
+      <h3 className="hero-title-large hero-title-sub">
+        I craft things for the web.
+      </h3>
+      <p className="hero-text">
+        I&apos;m a software engineer with expertise in creating top-notch
+        digital experiences. My current focus is on developing products that are
+        accessible and centered around user needs, as part of the team at&nbsp;
+        <Link href="https://rapidops.com" target="_blank" className="link">
+          Rapidops.
+        </Link>
+      </p>
+      <div className="hero-button">
+        <Button
+          text="Check out my course"
+          link="https://youtube.com/kishansheth21"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
