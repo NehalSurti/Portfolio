8225e2a27a77ae727d8bc35c38cacfb075a510b6
NehalSurti <nehalsurti9998@gmail.com>
2024-03-24 23:53:42 +0000

    Updating

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,176 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FiExternalLink, FiGithub } from "react-icons/fi";
+
+function Productspage() {
+  const projectsData = [
+    {
+      image: "/project1.png",
+      projectName: "Pokedex",
+      projectLink: "https://netlify.com",
+      projectDescription:
+        "This is a web application that uses the PokeAPI to display information about different Pokemon, including their evolution levels and details. You can compare multiple Pokemon using a queue, and add them to your personal list using Firebase's Firestore database.",
+      projectTech: [
+        "React",
+        "Redux Toolkit",
+        "SCSS",
+        "Firebase",
+        "Typescript",
+        "PokeApi",
+      ],
+      projectExternalLinks: {
+        github: "",
+        externalLink: "",
+      },
+    },
+    {
+      image: "/project2.png",
+      projectName: "Realtime Chat App",
+      projectLink: "https://netlify.com",
+      projectDescription:
+        "This is a chat app made with React and Node.js that sends real-time messages and is highly scalable. It uses advanced JavaScript concepts like debouncing and is optimized for React. It has 300+ stars on GitHub and over 180k views on YouTube.",
+      projectTech: [
+        "React",
+        "Sockets",
+        "Node.js",
+        "Express",
+        "MongoDB",
+        "Styled Components",
+      ],
+      projectExternalLinks: {
+        github: "",
+        externalLink: "",
+      },
+    },
+    {
+      image: "/project3.png",
+      projectName: "Netflix App",
+      projectLink: "https://netlify.com",
+      projectDescription:
+        "I made a Netflix copy with TMBD Api. It has infinite scrolling and lets you watch movies by genre. You can also add movies to your favorites list.",
+      projectTech: [
+        "React",
+        "Node.js",
+        "Firebase",
+        "MongoDB",
+        "Express",
+        "Redux Toolkit",
+      ],
+      projectExternalLinks: {
+        github: "",
+        externalLink: "",
+      },
+    },
+  ];
+  return (
+    <main>
+      <div className="projectspage">
+        <h2>Projects</h2>
+        <ul className="projects-grid">
+          {projectsData.map(
+            ({
+              image,
+              projectDescription,
+              projectLink,
+              projectExternalLinks,
+              projectName,
+              projectTech,
+            }) => {
+              return (
+                <li>
+                  <div className="project-container">
+                    <div className="project-image-overlay"></div>
+                    <div className="project-image-container">
+                      <Image src={image} fill alt={projectName} quality={100} />
+                    </div>
+                    <div className="project-info">
+                      <Link href="">
+                        <h2 className="project-info-title">{projectName}</h2>
+                      </Link>
+                      <p className="project-info-description">
+                        {projectDescription}
+                      </p>
+                    </div>
+                    <ul className="project-info-tech-list">
+                      {projectTech.map((tech) => (
+                        <li className="project-info-tech-list-item" key={tech}>
+                          {tech}
+                        </li>
+                      ))}
+                    </ul>
+                    <ul className="project-info-links">
+                      <li className="project-info-links-item">
+                        <Link
+                          href={projectExternalLinks.github}
+                          className="project-info-links-item-link"
+                        >
+                          <FiGithub />
+                        </Link>
+                      </li>
+                      <li className="project-info-links-item">
+                        <Link
+                          href={projectExternalLinks.externalLink}
+                          className="project-info-links-item-link"
+                        >
+                          <FiExternalLink />
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                //   <div
+                //     className="project"
+                //     key={projectName}
+
+                //   >
+                //     <div className="project-image">
+                //       <div className="project-image-overlay"></div>
+                //       <div className="project-image-container">
+                //         <Image src={image} fill alt={projectName} quality={100} />
+                //       </div>
+                //     </div>
+                //     <div className="project-info">
+                //       <p className="project-info-overline">Featured Project</p>
+                //       <h3 className="project-info-title">{projectName}</h3>
+                //       <div className="project-info-description">
+                //         <p>{projectDescription}</p>
+                //       </div>
+                //       <ul className="project-info-tech-list">
+                //         {projectTech.map((tech) => (
+                //           <li className="project-info-tech-list-item" key={tech}>
+                //             {tech}
+                //           </li>
+                //         ))}
+                //       </ul>
+                //       <ul className="project-info-links">
+                //         <li className="project-info-links-item">
+                //           <Link
+                //             href={projectExternalLinks.github}
+                //             className="project-info-links-item-link"
+                //           >
+                //             <FiGithub />
+                //           </Link>
+                //         </li>
+                //         <li className="project-info-links-item">
+                //           <Link
+                //             href={projectExternalLinks.externalLink}
+                //             className="project-info-links-item-link"
+                //           >
+                //             <FiExternalLink />
+                //           </Link>
+                //         </li>
+                //       </ul>
+                //     </div>
+                //   </div>
+              );
+            }
+          )}
+        </ul>
+      </div>
+      <div>page</div>;
+    </main>
+  );
+}
+
+export default Productspage;
