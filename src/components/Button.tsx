e815a4d68acaa99f95e1310645fd4bc08bd6e2a6
NehalSurti <nehalsurti9998@gmail.com>
2024-06-03 11:02:57 +0100

    Updating

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,16 @@ import Link from "next/link";
 import React from "react";
 
 function Button({ text, link }: { text: string; link: string }) {
+  const isMailtoLink = link.startsWith("mailto:");
+
+  if (isMailtoLink) {
+    return (
+      <a className="btn" href={link}>
+        {text}
+      </a>
+    );
+  }
+
   return (
     <Link className="btn" href={link}>
       {text}
